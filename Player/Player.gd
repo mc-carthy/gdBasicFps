81e7e92f652b5505956ad27ff3d0efea09245e54
Mick McCarthy <mick@mccarthy.tech>
2020-08-12 22:11:39 +0100

    Add gravity and jumping

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,7 +2,7 @@ extends KinematicBody
 
 export var speed: float = 10
 export var acceleration: float = 5
-export var gravity: float = -9.8
+export var gravity: float = -2.8
 export var jump_speed: float = 30
 export var mouse_sensitivity: float = 0.3
 
@@ -12,6 +12,7 @@ onready var camera: Camera = $Head/Camera
 var velocity := Vector3()
 var camera_vert: float = 0
 var camera_vert_limit: float = deg2rad(60)
+var is_jumping: bool = false
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -20,6 +21,9 @@ func _input(event: InputEvent) -> void:
 		var vert_delta = deg2rad(-event.relative.y * mouse_sensitivity)
 		camera_vert = clamp(camera_vert + vert_delta, -camera_vert_limit, camera_vert_limit)
 		camera.rotation.x = camera_vert
+
+func _process(delta: float) -> void:
+	is_jumping = true if Input.is_action_just_pressed('jump') else false
 
 func _physics_process(delta: float) -> void:
 	var head_basis: Basis = head.global_transform.basis
@@ -37,5 +41,9 @@ func _physics_process(delta: float) -> void:
 	direction = direction.normalized()
 	
 	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+	velocity.y += gravity
 	
-	velocity = move_and_slide(velocity)
+	if is_jumping and is_on_floor():
+		velocity.y += jump_speed
+	
+	velocity = move_and_slide(velocity, Vector3.UP)
