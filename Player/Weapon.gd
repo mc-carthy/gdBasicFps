e388b8b765187c9752fab7c2b1522c6ec701f024
Mick McCarthy <mick@mccarthy.tech>
2020-08-13 21:43:23 +0100

    Add reload button

--- Player/Weapon.gd
+++ Player/Weapon.gd
@@ -17,21 +17,27 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed('fire_primary') and can_fire:
-		fire()
+		if not is_reloading:
+			if current_ammo > 0:
+				fire()
+			else:
+				reload()
+	if Input.is_action_just_pressed('reload') and not is_reloading:
+		reload()
 
 func fire() -> void:
-	if not is_reloading:
-		if current_ammo > 0:
-			can_fire = false
-			current_ammo -= 1
-			check_collision()
-			yield(get_tree().create_timer(fire_rate), 'timeout')
-			can_fire = true
-		else:
-			is_reloading = true
-			yield(get_tree().create_timer(reload_speed), 'timeout')
-			is_reloading = false
-			current_ammo = clip_size
+		can_fire = false
+		current_ammo -= 1
+		ammo_label.set_text('Ammo: %d/%d' % [current_ammo, clip_size])
+		check_collision()
+		yield(get_tree().create_timer(fire_rate), 'timeout')
+		can_fire = true
+
+func reload() -> void:
+	is_reloading = true
+	yield(get_tree().create_timer(reload_speed), 'timeout')
+	is_reloading = false
+	current_ammo = clip_size
 	ammo_label.set_text('Ammo: %d/%d' % [current_ammo, clip_size])
 
 func check_collision() -> void:
