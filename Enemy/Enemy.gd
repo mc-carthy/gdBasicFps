0205d96206f84872fd103c859b4007d7817914ae
Mick McCarthy <mick@mccarthy.tech>
2020-08-23 22:20:34 +0100

    Add player tracking when visible

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,15 +1,23 @@
 extends KinematicBody
 
+var target: Spatial
 
+func _process(delta: float) -> void:
+	if target:
+		look_at(target.global_transform.origin, Vector3.UP)
 
 func _on_Area_body_entered(body: Node) -> void:
 	print(body.name + " entered")
-	set_colour_red()
+	if body.is_in_group('Player'):
+		target = body
+		set_colour_red()
 
 
 func _on_Area_body_exited(body: Node) -> void:
 	print(body.name + " exited")
-	set_colour_green()
+	if body.is_in_group('Player'):
+		target = null
+		set_colour_green()
 
 func set_colour_red() -> void:
 	$MeshInstance.get_surface_material(0).set_albedo(Color(1, 0, 0))
