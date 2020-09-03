a66dde3aa1947a3c50730740ea9c4916d09163c0
Mick McCarthy <mick@mccarthy.tech>
2020-09-03 22:33:39 +0100

    Add basic enemy chase behaviour

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,5 +1,7 @@
 extends KinematicBody
 
+export var speed: float = 100
+
 var target: Spatial
 var space_state: PhysicsDirectSpaceState
 
@@ -8,12 +10,17 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if target:
-		var result = space_state.intersect_ray(global_transform.origin, target.global_transform.origin)
+		var result: Dictionary = space_state.intersect_ray(global_transform.origin, target.global_transform.origin)
 		if result.collider.is_in_group('Player'):
 			look_at(target.global_transform.origin, Vector3.UP)
 			set_colour_red()
+			move_to_target(delta)
 		else:
 			set_colour_green()
+
+func move_to_target(delta: float) -> void:
+	var dir: Vector3 = (target.global_transform.origin - global_transform.origin).normalized()
+	move_and_slide(dir * speed * delta, Vector3.UP)
 
 func _on_Area_body_entered(body: Node) -> void:
 	print(body.name + " entered")
