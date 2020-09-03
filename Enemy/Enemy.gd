2fdee291b356d4d06f17c294209f5a289834cd22
Mick McCarthy <mick@mccarthy.tech>
2020-09-03 22:25:46 +0100

    Add LOS ray check

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,10 +1,19 @@
 extends KinematicBody
 
 var target: Spatial
+var space_state: PhysicsDirectSpaceState
+
+func _ready() -> void:
+	space_state = get_world().direct_space_state
 
 func _process(delta: float) -> void:
 	if target:
-		look_at(target.global_transform.origin, Vector3.UP)
+		var result = space_state.intersect_ray(global_transform.origin, target.global_transform.origin)
+		if result.collider.is_in_group('Player'):
+			look_at(target.global_transform.origin, Vector3.UP)
+			set_colour_red()
+		else:
+			set_colour_green()
 
 func _on_Area_body_entered(body: Node) -> void:
 	print(body.name + " entered")
